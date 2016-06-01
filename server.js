8946b1e364593643a79fa5f9a757c70a69b83491
Kyoera Fox <truc93phd@gmail.com>
2016-06-02 03:05:32 +0700

    server.js

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
-var express = require ('express'),
-	http = require('http'),
-	path = require('path');
-
+var express = require('express');
 var app = express();
+var http = require('http');
+var path = require('path');
 
-app.configure(function(req, res){});
-app.set('port', (process.env.PORT || 8080));
-app.set('views', __dirname + 'views');
-app.set('view engine', 'html');
-//app.use(express.static(path.join(__dirname, 'public')));
+app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function(request, response) {
-  console.log('abc');
+app.use(express.static(__dirname + '/views'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+
+app.get("/", function(request, response) {
+    response.sendFile("home.html", {root: "./views"});
 });
 
-
-http.createServer(app).listen(app.get('port'), function(){
-  	console.log("Server listening on port " + app.get('port'));
+app.use(function(req, res, next){
+	if(res.status(404)){
+		res.sendFile("404.html", {root: "./views"});
+	}
 });
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
 
