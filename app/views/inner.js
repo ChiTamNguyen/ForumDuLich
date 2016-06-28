9b870d2208568c2537e15ebba989847bf8a880bd
ChiTamNguyen <1312507@student.hcmus.edu.vn>
2016-06-28 21:12:15 +0700

    fixed log in

---
+++ app/views/inner.js
@@ -0,0 +1,3 @@
+var element = document.getElementById('menu_footer');
+console.log(element);
+console.log(element.innerHTML);
---
+++ controllers/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+	'url' : 'mongodb://ginta12006:ginta12000@ds023684.mlab.com:23684/forumtravel'
+};
