7dcf4d17bf8360f2d02ca641a61f16613b9b8bde
ChiTamNguyen <1312507@student.hcmus.edu.vn>
2016-06-29 22:22:50 +0700

    Update

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,6 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '1074205052655644', // your App ID
         'clientSecret'  : 'bb6d3a9da8006aa184c5d34ce7bbb5cc', // your App Secret
-        'callbackURL'   : 'http://localhost:5000/auth/facebook/callback'
+        'callbackURL'   : 'http://dulich.herokuapp.com/auth/facebook/callback'
     }
 };
